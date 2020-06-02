39709a09f9a1ba70afce13d992d60ed800b9b3f5
Li Kai <likai@nasetech.com>
2020-06-02 18:16:56 +0800

    修改unzip为  tar

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "安装中"
 curl -s https://codeload.github.com/nasetech/standard-setup/zip/master --output /tmp/standard-setup.zip
-unzip -a /tmp/standard-setup.zip  -d .
+tar -zxvf /tmp/standard-setup.zip  -C .
 mv -f standard-setup-master standard-setup
 echo "安装完毕"
